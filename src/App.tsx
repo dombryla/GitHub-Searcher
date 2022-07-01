45f1627c116d5850c7304f8526a68b49c4fcf5d0
dombryla <dombryla@interia.pl>
2022-07-01 21:01:13 +0200

    Create simple form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useFela, Style } from './styling'
+import { useForm, SubmitHandler } from "react-hook-form";
 
 
+type SearchInputs = {
+  searchPhrase: string,
+  userName: string,
+  language: "js" | "go" | "java"
+}
 export const App: React.FC = () => {
 
   const {
     css,
     theme: { colors, fontSize, spacing },
   } = useFela()
+
+  const { register, handleSubmit,  formState: { errors } } = useForm<SearchInputs>();
 
   const rule: Style = {
     backgroundColor: colors.backgroundColor.main,
@@ -19,6 +27,10 @@ export const App: React.FC = () => {
     color: colors.fontColor.main
   }
 
+  const onSubmit: SubmitHandler<SearchInputs> = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className={css(rule)}>
       <div
@@ -28,8 +40,19 @@ export const App: React.FC = () => {
         })}
       >
         <div>GitHub Searcher</div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor='searchPhrase'>
+            Search phrase
+            <input {...register("searchPhrase", {required: "This field is required."})} id="searchPhrase"></input>
+          </label>
+          <p>{errors.searchPhrase?.message}</p>
+          <input {...register("userName", {required: "This field is required."})}></input>
+          <p>{errors.userName?.message}</p>
+          <button>Search</button>
+        </form>
       </div>
     </div>
   );
 }
+
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+
 import { App } from './App'
 import { FelaProvider, createRenderer } from './styling'
 
 const renderer = createRenderer()
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <FelaProvider renderer={renderer}>
       <App />
     </FelaProvider>
   </React.StrictMode>,
-  document.getElementById('root')
 )
+
