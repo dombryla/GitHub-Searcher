87cea924526dfacef752c661ec7208f08ffcf55e
dombryla <dombryla@interia.pl>
2022-07-01 19:24:18 +0200

    Add basic application styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,32 @@ import { useFela, Style } from './styling'
 
 
 export const App: React.FC = () => {
-  const {css} = useFela()
+
+  const {
+    css,
+    theme: { colors, fontSize, spacing },
+  } = useFela()
+
+  const rule: Style = {
+    backgroundColor: colors.backgroundColor.main,
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: fontSize.xl,
+    color: colors.fontColor.main
+  }
 
   return (
-    <div className={css({backgroundColor: "red"})}>
-        GitHub Searcher
+    <div className={css(rule)}>
+      <div
+        className={css({
+          margin: `${spacing.s02}`,
+          height: 'inherit',
+        })}
+      >
+        <div>GitHub Searcher</div>
+      </div>
     </div>
   );
 }
--- src/styling/colors.ts
+++ src/styling/colors.ts
@@ -1,9 +1,9 @@
 const backgroundColor = {
-
+  main: "#161B22"
 }
 
 const fontColor = {
-
+  main: "#C3CBD9"
 }
 
 export const colors = {
