ed24c9dd34999cb19496d19bd4378d0ec4e11f10
dombryla <dombryla@interia.pl>
2022-07-02 14:26:41 +0200

    Add Avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFela, Style, spacing } from './styling'
+import { useFela, Style } from './styling'
 import { Form } from "./components/form"
 import { Panel } from "./components/panel"
 import { search } from './api';
@@ -7,12 +7,16 @@ import { SearchInput, SearchItem } from './model/models';
 import { SubmitHandler } from "react-hook-form"
 import { useAsyncCallback } from 'react-async-hook';
 import { Link } from './components/link';
+import { Avatar } from './components/avatar';
 
 
 export const App: React.FC = () => {
+
+  const [showAvatar, setShowAvatar] = React.useState(false)
+
   const {
     css,
-    theme: { colors, fontSize },
+    theme: { colors, fontSize, spacing },
   } = useFela()
 
 
@@ -38,50 +42,58 @@ export const App: React.FC = () => {
     borderCollapse: "collapse"
   }
 
+  const showModal = (): void =>{
+    setShowAvatar(true)
+  }
 
-  console.log(data)
+  const hideModal = (): void => {
+    setShowAvatar(false)
+  }
+
+
   return (
     <div className={css(rule)}>
-        <div className={css({fontSize: fontSize.xl4})}>GitHub Searcher</div>
-        <Panel>
-            <Form onSubmit={onClick}/>
-        </Panel>
-        <Panel backgroundColor={colors.background.main}>
-
+      <div className={css({fontSize: fontSize.xl4})}>GitHub Searcher</div>
+      <Panel>
+        <Form onSubmit={onClick}/>
+      </Panel>
+      <Panel backgroundColor={colors.background.main}>
         {data.status === "success" ? 
         <table className={css(tableStyle)}>
-          <thead
-          className={css({
+          <thead className={css({
             opacity: "1",
             fontSize: fontSize.xl2,
             backgroundColor: colors.background.second
           })}
-        >
-          <tr>
-            <th>Name </th>
-            <th>Description</th>
-            <th>User</th>
-          </tr>
-        </thead>
-        <div className={css({padding: spacing.s025})}></div>
-        <tbody className={css({textAlign: "center"})}>
-          {data.result!.map((item: SearchItem) => {
-            return(
-              <tr className={css({
-                borderBottom: `1px solid ${colors.background.second}`})}>
-                <td>
-                  <Link url={item.url} title={item.name} />
-                </td>
-                <td>{item.description}</td>
-                <td>{item.login}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>:
-      <div>brak danych</div>
-      }
-        </Panel>
+          >
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>User</th>
+            </tr>
+          </thead>
+          <div className={css({padding: spacing.s025})}></div>
+          <tbody className={css({textAlign: "center"})}>
+            {data.result!.map((item: SearchItem, i: number) => {
+              return(
+                <tr className={css({borderBottom: `1px solid ${colors.background.second}`})} key={i}>
+                  <td>
+                    <Link url={item.url} title={item.name} />
+                  </td>
+                  <td>{item.description}</td>
+                  <td onClick={showModal}>
+                    {item.login}
+                  </td>
+                  {showAvatar && <Avatar url={item.avatarUrl}  onClick={hideModal} />}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>:
+        <div>brak danych</div>
+        }
+      </Panel>
+
     </div>
   );
 }
---
+++ src/components/avatar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useFela, Style } from '../styling'
+
+export type AvatarProps = {
+    url: string
+    onClick: () =>void
+  }
+  
+export const Avatar: React.FC<AvatarProps> = ({url, onClick}) =>{
+const { css, theme:{colors, border, spacing, borderRadius} } = useFela()
+    
+const rule: Style = {
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: " translate(-50%, -50%)",
+  zIndex: 10,
+  padding: spacing.s04,
+  border: border.main,
+  borderRadius: borderRadius.xl2,
+  boxShadow: `0px 0px 11px -3px ${colors.background.input}`,
+  backgroundColor: colors.background.second,
+}
+  
+  return(
+    <div className={css(rule)}>
+      <img src={url} alt="avatar"></img>
+      <button onClick={onClick}>Close</button>
+    </div>
+  )
+}
